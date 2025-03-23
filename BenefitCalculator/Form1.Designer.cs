610b19ab4b28f43e4ccce8a9abc14ade66d5aa61
Yura <patrunyura@gmail.com>
2025-03-23 15:42:41 +0200

    add all functionality

--- BenefitCalculator/Form1.Designer.cs
+++ BenefitCalculator/Form1.Designer.cs
@@ -1,38 +1,97 @@
-﻿namespace BenefitCalculator;
-
-partial class Form1
+﻿﻿namespace BenefitCalculator
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label lblFamilyMembers;
+        private System.Windows.Forms.TextBox txtFamilyMembers;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label lblElectricity;
+        private System.Windows.Forms.TextBox txtElectricity;
+        private System.Windows.Forms.Label lblGas;
+        private System.Windows.Forms.TextBox txtGas;
+        private System.Windows.Forms.Label lblWater;
+        private System.Windows.Forms.TextBox txtWater;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblResult;
+        
+        private void InitializeComponent()
         {
-            components.Dispose();
+            int labelWidth = 180;
+            int inputWidth = 150;
+            int startX = 10;
+            int inputX = startX + labelWidth + 10;
+            int rowHeight = 30;
+            
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lblFamilyMembers = new System.Windows.Forms.Label();
+            this.txtFamilyMembers = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.lblElectricity = new System.Windows.Forms.Label();
+            this.txtElectricity = new System.Windows.Forms.TextBox();
+            this.lblGas = new System.Windows.Forms.Label();
+            this.txtGas = new System.Windows.Forms.TextBox();
+            this.lblWater = new System.Windows.Forms.Label();
+            this.txtWater = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            
+            // Labels and Inputs
+            System.Windows.Forms.Label[] labels = { lblCategory, lblFamilyMembers, lblElectricity, lblGas, lblWater };
+            System.Windows.Forms.Control[] inputs = { cmbCategory, txtFamilyMembers, txtElectricity, txtGas, txtWater };
+            
+            string[] labelTexts = {
+                "Категорія пільговика:", "Кількість членів сім'ї:",
+                "Витрати електроенергії (кВт):", "Витрати газу (м³):", "Витрати води (м³):"
+            };
+            
+            for (int i = 0; i < labels.Length; i++)
+            {
+                labels[i].Text = labelTexts[i];
+                labels[i].Location = new System.Drawing.Point(startX, 10 + i * rowHeight);
+                labels[i].Size = new System.Drawing.Size(labelWidth, 20);
+                labels[i].AutoSize = false;
+                
+                inputs[i].Location = new System.Drawing.Point(inputX, 10 + i * rowHeight);
+                inputs[i].Size = new System.Drawing.Size(inputWidth, 22);
+            }
+            
+            this.cmbCategory.Items.AddRange(new object[] { "Одинокий інвалід", "Ветеран УПА (75%)", "Особа з інвалідністю (50%)" });
+            
+            // Buttons
+            this.btnCalculate.Text = "Розрахувати";
+            this.btnCalculate.Location = new System.Drawing.Point(startX, 200);
+            this.btnCalculate.Size = new System.Drawing.Size(100, 30);
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            
+            this.btnClear.Text = "Очистити";
+            this.btnClear.Location = new System.Drawing.Point(inputX, 200);
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            
+
+            
+            // Result label
+            this.lblResult.Location = new System.Drawing.Point(startX, 240);
+            this.lblResult.Size = new System.Drawing.Size(300, 30);
+            
+            // Form1 settings
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.AddRange(labels);
+            this.Controls.AddRange(inputs);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Text = "Розрахунок пільг";
+            this.ResumeLayout(false);
         }
-        base.Dispose(disposing);
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- BenefitCalculator/Form1.cs
+++ BenefitCalculator/Form1.cs
@@ -1,9 +1,64 @@
-namespace BenefitCalculator;
+using System;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace BenefitCalculator
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string category = cmbCategory.SelectedItem.ToString();
+                int familyMembers = int.Parse(txtFamilyMembers.Text);
+                double electricity = double.Parse(txtElectricity.Text);
+                double gas = double.Parse(txtGas.Text);
+                double water = double.Parse(txtWater.Text);
+
+                double discount = 0;
+                if (category == "Одинокий інвалід")
+                    discount = 1.0; // 100%
+                else if (category == "Особа з інвалідністю (50%)")
+                    discount = 0.5; // 50%
+                else if (category == "Ветеран УПА (75%)")
+                    discount = 0.25; // 75%
+
+                // double totalBeforeDiscount = (electricity * 1.44) + (gas * 7) + (water * 10) + (area * 20);
+                // double totalCost = totalBeforeDiscount * (1 - discount);
+
+                double totalBeforeDiscount = (electricity * 4.32) + (gas * 8) + (water * 90);
+
+                double totalCost;
+                if (discount == 1.0)
+                {
+                    totalCost = totalBeforeDiscount; // Повне повернення коштів
+                }
+                else
+                {
+                    totalCost = totalBeforeDiscount * (1 - discount);
+                }
+
+                lblResult.Text = $"Сума до відшкодування: {totalCost:F2} грн";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка! Переконайтесь, що введені дані правильні.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            cmbCategory.SelectedIndex = -1;
+            txtFamilyMembers.Text = "";
+            txtElectricity.Text = "";
+            txtGas.Text = "";
+            txtWater.Text = "";
+            lblResult.Text = "";
+        }
     }
 }
